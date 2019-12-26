255699003c2116a44a4431714ff6b0eed4d9cd20
Joshua Shaffer <jshaffer189@gmail.com>
2019-12-25 18:54:17 -0600

    missing commit

---
+++ Section12/objectmethods.js
@@ -0,0 +1,16 @@
+// Adding custom built functions inside objects key: func()
+
+// const ADD = function(x, y) {
+// 	return x + y;
+// };
+
+// accessed MATH.key(arg, arg)
+const MATH = {
+	numbers: [ 1, 2, 3, 4, 5 ],
+	add(x, y) {
+		return x + y;
+	},
+	multiply(x, y) {
+		return x * y;
+	}
+};
