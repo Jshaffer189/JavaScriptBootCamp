a0b6e254deaaa44715e0083be30a9ec1088b731a
Joshua Shaffer <jshaffer189@gmail.com>
2019-12-26 20:00:44 -0600

    moving to laptop

--- Section12/this.js
+++ Section12/this.js
@@ -5,3 +5,26 @@ function sayHi() {
 	console.log(this);
 }
 sayHi();
+
+const person = {
+	first: 'Cherilyn',
+	last: 'Sarkisian',
+	nickName: 'Cher',
+	fullName() {
+		// person object
+		console.log(this);
+		// Cherilyn
+		console.log(this.first);
+		// Cherilyn Sarkisian AKA Cher
+		// Destructure and group
+		const { first, last, nickName } = this;
+		return `${first} ${last} AKA ${nickName}`;
+	},
+	printBio() {
+		const FULLNAME = this.fullName();
+		console.log(`${FULLNAME} is a person!`);
+	}
+};
+
+// Cherilyn Sarkisian AKA Cher is a person!
+person.printBio();
