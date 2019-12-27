9f70472f5c98a278e1c48e94263c984460ee95a2
joshuashaffer189 <jshaffer189@gmail.com>
2019-12-26 23:32:52 -0600

    still covering this keyword

---
+++ Section12/deckofcards.js
@@ -0,0 +1,5 @@
+function makeDeck() {
+	const DECK = [];
+	const SUITS = [ 'hearts', 'diamonds', 'spades', 'clubs' ];
+	const VALUES = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ];
+}
