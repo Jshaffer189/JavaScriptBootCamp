a52ade16f2ddc6eda431cc67cd0c4a14c7f761cf
joshuashaffer189 <jshaffer189@gmail.com>
2019-12-27 23:06:46 -0600

    finished object less deck of cards

--- Section12/annoyomaticdemo.js
+++ Section12/annoyomaticdemo.js
@@ -16,4 +16,4 @@ const annoyer = {
 	}
 };
 
-annoyer.start();
+// annoyer.start();
--- Section12/deckofcards.js
+++ Section12/deckofcards.js
@@ -1,5 +1,51 @@
-function makeDeck() {
-	const DECK = [];
-	const SUITS = [ 'hearts', 'diamonds', 'spades', 'clubs' ];
-	const VALUES = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ];
+// create deck
+function initializeDeck() {
+	deck = [];
+	drawnCards = [];
+	suits = [ 'hearts', 'diamonds', 'spades', 'clubs' ];
+	values = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ];
+	const { suits, values, deck } = this;
+	for (let value of values) {
+		for (let suit of suits) {
+			deck.push({ value, suit });
+		}
+	}
+	return deck;
 }
+
+// draw single card
+function drawCard(deck, drawnCards) {
+	const card = deck.pop();
+	drawnCards.push(card);
+	return card;
+}
+
+// draw multiple cards
+function drawMultiple(numCards, deck, drawnCards) {
+	const cards = [];
+	for (let i = 0; i < numCards; i++) {
+		// draw a number cards per loop, push and return the array of cards drawn
+		cards.push(drawCard(deck, drawnCards));
+	}
+	return cards;
+}
+
+// shuffle deck
+function shuffle(deck) {
+	const { deck } = this;
+	// loop over array backwards
+	for (let i = deck.length - 1; i > 0; i--) {
+		// pick random index before current element
+		let j = Math.floor(Math.random() * (i + 1));
+		// numbers index'es swap using destructuring
+		[ deck[i], deck[j] ] = [ deck[j], deck[i] ];
+	}
+	return deck;
+}
+
+const firstDeck = initializeDeck();
+const drawnCards = [];
+shuffle(firstDeck);
+const hand1 = drawMultiple(2, firstDeck, drawnCards);
+const hand2 = drawMultiple(2, firstDeck, drawnCards);
+const pokerHand = drawMultiple(5, firstDeck, drawnCards);
