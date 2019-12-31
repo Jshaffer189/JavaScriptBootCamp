334ab6fa62648cc87fddc8584092316c935ac1cd
joshuashaffer189 <jshaffer189@gmail.com>
2019-12-31 03:39:57 -0600

    multi events

---
+++ Section15/multievents/app.js
@@ -0,0 +1,9 @@
+const colors = [ 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'indigo', 'violet' ];
+
+const container = document.querySelector('#boxes');
+for (let color of colors) {
+	const box = document.createElement('div');
+	box.style.backgroundColor = color;
+	box.classList.add('box');
+	container.append(box);
+}
