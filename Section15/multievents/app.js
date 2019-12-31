a63a40fc35327a3e4717d9746ac386fd865fd226
joshuashaffer189 <jshaffer189@gmail.com>
2019-12-31 04:16:37 -0600

    moving on to event object

--- Section15/multievents/app.js
+++ Section15/multievents/app.js
@@ -1,9 +1,23 @@
 const colors = [ 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'indigo', 'violet' ];
 
+const changeColor = function() {
+	const h1 = document.querySelector('h1');
+	h1.style.color = this.style.backgroundColor;
+
+	// this - the box object
+	console.log(this);
+	// this.backgroundcolor of box object
+	console.log(this.style.backgroundColor);
+};
+
 const container = document.querySelector('#boxes');
 for (let color of colors) {
 	const box = document.createElement('div');
 	box.style.backgroundColor = color;
 	box.classList.add('box');
-	container.append(box);
+	container.appendChild(box);
+	// box.addEventListener('click', function() {
+	// 	printColor(box);
+	// });
+	box.addEventListener('click', changeColor);
 }
