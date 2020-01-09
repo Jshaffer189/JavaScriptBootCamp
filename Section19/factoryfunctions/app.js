d5ebbeafb203571ae5925f060d5d61f31cb4e8eb
joshuashaffer189 <jshaffer189@gmail.com>
2020-01-08 21:49:24 -0600

    upkeep

---
+++ Section19/factoryfunctions/app.js
@@ -0,0 +1,12 @@
+function hex(r, g, b) {
+	return '#' + ((1 << 24) + (r << 16) + (g << 8).toString(16).slice(1));
+}
+function rgb() {}
+
+hex(255, 100, 25);
+
+function makeColor(r, g, b) {
+    const color = {};
+    color.r = 
+	return color;
+}
