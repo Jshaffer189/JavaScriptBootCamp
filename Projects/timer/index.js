e3b52e4b6ccaee26c73c695a5f20c556c0b4eaea
Joshua Shaffer <jshaffer189@gmail.com>
2020-01-12 11:13:34 -0600

    wiring up callbacks for timer css functionality

--- Projects/timer/index.js
+++ Projects/timer/index.js
@@ -1,8 +1,11 @@
 class Timer {
-	constructor(durationInput, startButton, pauseButton) {
+	constructor(durationInput, startButton, pauseButton, callbacks) {
 		this.durationInput = durationInput;
 		this.startButton = startButton;
 		this.pauseButton = pauseButton;
+		if (callbacks) {
+			this.onStart = callbacks.onStart;
+		}
 
 		// eventlistener to start count
 		this.startButton.addEventListener('click', this.start);
@@ -10,7 +13,12 @@ class Timer {
 	}
 
 	start = () => {
+		if (this.onStart) {
+			// if callback was passed through constructor call onStart() function
+			this.onStart();
+		}
 		this.tick();
+		// calling tick every second until paused
 		this.interval = setInterval(this.tick, 1000);
 	};
 
@@ -19,8 +27,23 @@ class Timer {
 	};
 
 	tick = () => {
-		console.log('tick');
+		if (this.timeRemaining <= 0) {
+			this.pause();
+		} else {
+			// Left setter = getter right
+			this.timeRemaining = this.timeRemaining - 1;
+		}
 	};
+
+	// getter, retrive time remaining
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value);
+	}
+
+	// setter, time being 'this'
+	set timeRemaining(time) {
+		this.durationInput.value = time;
+	}
 }
 
 // btn selectors
@@ -28,5 +51,12 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-// class calls
-const timer = new Timer(durationInput, startButton, pauseButton);
+// class/constructor call
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	// optional call back functions
+	onStart() {
+		console.log('timer started');
+	},
+	onTick() {},
+	onComplete() {}
+});
