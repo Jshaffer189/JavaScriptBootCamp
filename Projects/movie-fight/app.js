3abf29ad83aac8a82f59cd90c159a998ebb61dbe
Joshua Shaffer <jshaffer189@gmail.com>
2020-01-15 15:06:28 -0600

    finished debounce

--- Projects/movie-fight/app.js
+++ Projects/movie-fight/app.js
@@ -1,3 +1,6 @@
+// DOM Element Selectors
+const input = document.querySelector('input');
+
 const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
@@ -8,9 +11,23 @@ const fetchData = async (searchTerm) => {
 	console.log(response.data);
 };
 
+// debounce helper
+const debounce = (func, delay = 1000) => {
+	let timeoutId;
+	return (...args) => {
+		// debouncing the text input
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+			func.apply(null, args);
+		}, delay);
+	};
+};
 
+const onInput = (event) => {
+	fetchData(event.target.value);
+};
 
-const input = document.querySelector('input');
-input.addEventListener('input', (event) => {
-	fetchData(event.target.value)
-})
+// Event Listeners
+input.addEventListener('input', debounce(onInput, 500));
