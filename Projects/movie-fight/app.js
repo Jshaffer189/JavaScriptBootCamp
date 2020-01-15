caf872f2ba254fcd2618a1d54e3df9d1ac2a980a
Joshua Shaffer <jshaffer189@gmail.com>
2020-01-15 10:47:41 -0600

    working on delaying button press

--- Projects/movie-fight/app.js
+++ Projects/movie-fight/app.js
@@ -1,11 +1,16 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: 'f241bf34',
-			s: 'avengers'
+			s: searchTerm
 		}
 	});
 	console.log(response.data);
 };
 
-fetchData();
+
+
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+	fetchData(event.target.value)
+})
