1873c41517fddc2fede76e71c25ff62a3458077f
joshuashaffer189 <jshaffer189@gmail.com>
2020-01-16 20:54:50 -0600

    image error

--- Projects/movie-fight/app.js
+++ Projects/movie-fight/app.js
@@ -13,7 +13,18 @@ const fetchData = async (searchTerm) => {
 
 const onInput = async (event) => {
 	const movies = await fetchData(event.target.value);
-	console.log(movies);
+
+	for (let movie of movies) {
+		const div = document.createElement('div');
+
+		// image error
+		div.innerHTML = `
+			<div src="${movie.Poster}" />
+			<h1>${movie.Title}</h1>
+		`;
+
+		document.querySelector('#target').appendChild(div);
+	}
 };
 
 // Event Listeners
