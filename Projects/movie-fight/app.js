da0df3d4be633eda4c2c6de1bb1bda0d5cad2cbf
Joshua Shaffer <jshaffer189@gmail.com>
2020-01-17 11:25:53 -0600

    dropdown more functional

--- Projects/movie-fight/app.js
+++ Projects/movie-fight/app.js
@@ -1,24 +1,7 @@
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-	<label><b>Search For a Movie</b></label>
-	<input class="input" />
-	<div class="dropdown is-active">
-		<div class="dropdown-menu">
-			<div class="dropdown-content results"></div>
-		</div>
-	</div>
-`;
-
-// DOM Element Selectors
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-// Axios API
 const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
-			apikey: 'f241bf34',
+			apikey: 'd9835cc5',
 			s: searchTerm
 		}
 	});
@@ -30,21 +13,47 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+	<label><b>Search For a Movie</b></label>
+	<input class="input" />
+	<div class="dropdown">
+	  <div class="dropdown-menu">
+		<div class="dropdown-content results"></div>
+	  </div>
+	</div>
+  `;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 const onInput = async (event) => {
 	const movies = await fetchData(event.target.value);
 
+	if (!movies.length) {
+		dropdown.classList.remove('is-active');
+		return;
+	}
+
+	dropdown.classList.add('is-active');
 	for (let movie of movies) {
-		const div = document.createElement('div');
+		const option = document.createElement('a');
 
-		// image error
-		div.innerHTML = `
-			<img src="${movie.Poster}" />
-			<h1>${movie.Title}</h1>
-		`;
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
+		<img src="${movie.Poster}" />
+		${movie.Title}
+	  `;
 
-		document.querySelector('#target').appendChild(div);
+		resultsWrapper.appendChild(option);
 	}
 };
 
 // Event Listeners
-input.addEventListener('input', debounce(onInput, 1000));
+input.addEventListener('input', debounce(onInput, 500));
+document.addEventListener('click', (event) => {
+	if (!root.contains(event.target)) {
+		dropdown.classList.remove('is-active');
+	}
+});
