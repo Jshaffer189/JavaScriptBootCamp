006327e5e9cdff855cd0f5a6b788ffeae9b8d769
Joshua Shaffer <jshaffer189@gmail.com>
2020-01-17 19:29:56 -0600

    finished debugging

--- Projects/movie-fight/app.js
+++ Projects/movie-fight/app.js
@@ -36,24 +36,64 @@ const onInput = async (event) => {
 		return;
 	}
 
+	resultsWrapper.innerHTML = '';
 	dropdown.classList.add('is-active');
 	for (let movie of movies) {
 		const option = document.createElement('a');
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
 		option.classList.add('dropdown-item');
 		option.innerHTML = `
-		<img src="${movie.Poster}" />
-		${movie.Title}
-	  `;
+			<img src="${imgSrc}" />
+			${movie.Title}
+		`;
 
+		// Generated Event Listener
+		option.addEventListener('click', () => {
+			dropdown.classList.remove('is-active');
+			input.value = movie.Title;
+			onMovieSelect(movie);
+		});
+
+		// final appending
 		resultsWrapper.appendChild(option);
 	}
 };
 
 // Event Listeners
 input.addEventListener('input', debounce(onInput, 500));
+// menu out click
 document.addEventListener('click', (event) => {
 	if (!root.contains(event.target)) {
 		dropdown.classList.remove('is-active');
 	}
 });
+
+const onMovieSelect = async (movie) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'd9835cc5',
+			i: movie.imdbID
+		}
+	});
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+	return `
+		<article class="media">
+			<figure class="media-left">
+				<p class="image">
+					<img src="${movieDetail.Poster}" >
+				</p>
+			</figure>
+			<div class="media-content">
+				<div class="content">
+					<h1>${movieDetail.Title}</h1>
+					<h4>${movieDetail.Genre}</h4>
+					<p>${movieDetail.Plot}</p>
+				</div>
+			</div>
+		</article>
+	`;
+};
