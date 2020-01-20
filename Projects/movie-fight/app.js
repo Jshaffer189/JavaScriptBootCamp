09b2918d4f08bd1227b871dca86a5425a1c479b4
Joshua Shaffer <jshaffer189@gmail.com>
2020-01-19 19:01:03 -0600

    reconfigure autocomplete

--- Projects/movie-fight/app.js
+++ Projects/movie-fight/app.js
@@ -13,59 +13,14 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-	<label><b>Search For a Movie</b></label>
-	<input class="input" />
-	<div class="dropdown">
-	  <div class="dropdown-menu">
-		<div class="dropdown-content results"></div>
-	  </div>
-	</div>
-  `;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (event) => {
-	const movies = await fetchData(event.target.value);
-
-	if (!movies.length) {
-		dropdown.classList.remove('is-active');
-		return;
-	}
-
-	resultsWrapper.innerHTML = '';
-	dropdown.classList.add('is-active');
-	for (let movie of movies) {
-		const option = document.createElement('a');
+createAutoComplete({
+	root: document.querySelector('.autocomplete'),
+	renderOption(movie) {
 		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-		option.classList.add('dropdown-item');
-		option.innerHTML = `
-			<img src="${imgSrc}" />
-			${movie.Title}
-		`;
-
-		// Generated Event Listener
-		option.addEventListener('click', () => {
-			dropdown.classList.remove('is-active');
-			input.value = movie.Title;
-			onMovieSelect(movie);
-		});
-
-		// final appending
-		resultsWrapper.appendChild(option);
-	}
-};
-
-// Event Listeners
-input.addEventListener('input', debounce(onInput, 500));
-// menu out click
-document.addEventListener('click', (event) => {
-	if (!root.contains(event.target)) {
-		dropdown.classList.remove('is-active');
+		return `
+                <img src="${imgSrc}" />
+                ${movie.Title} (${movie.Year})
+		    `;
 	}
 });
 
@@ -76,6 +31,7 @@ const onMovieSelect = async (movie) => {
 			i: movie.imdbID
 		}
 	});
+	// Movie Summary Element
 	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
 };
 
