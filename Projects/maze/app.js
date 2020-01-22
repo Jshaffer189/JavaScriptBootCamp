54e81ad55ab2326629a1c9e673376ef3f7ae43e2
Joshua Shaffer <jshaffer189@gmail.com>
2020-01-21 21:38:02 -0600

    beginning maze

--- Projects/maze/app.js
+++ Projects/maze/app.js
@@ -1,9 +1,12 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 // Engine -
 // Render -
 // Runner -
 // World - access to current app/game state
 // Bodies - access to creating shape elements
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -11,14 +14,35 @@ const render = Render.create({
 	element: document.body,
 	engine: engine,
 	options: {
-		width: 800,
-		height: 600
+		wireframes: false,
+		width: width,
+		height: height
 	}
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-	isStatic: true
-});
-World.add(world, shape);
+World.add(
+	world,
+	MouseConstraint.create(engine, {
+		mouse: Mouse.create(render.canvas)
+	})
+);
+
+// Rectangle border Walls
+const walls = [
+	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+	Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+	Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+
+World.add(world, walls);
+for (let i = 0; i < 40; i++) {
+	if (Math.random() > 0.5) {
+		// Random Shapes
+		World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
+	} else {
+		World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35));
+	}
+}
