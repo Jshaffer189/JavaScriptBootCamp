e3317f557c6ad845892fa27e42032fdacc001ec3
Joshua Shaffer <jshaffer189@gmail.com>
2020-02-18 22:13:48 -0600

    unable to implement chalk on my git

--- Projects/node-cmd/list/index.js
+++ Projects/node-cmd/list/index.js
@@ -5,8 +5,9 @@
 
 const fs = require('fs');
 const util = require('util');
+const chalk = require('chalk');
 
-const lstat = util.promisify(fs.lstat);
+// const lstat = util.promisify(fs.lstat);
 
 const { lstat } = fs.promises;
 
@@ -15,26 +16,19 @@ fs.readdir(process.cwd(), async (err, filenames) => {
 		console.log(err);
 	}
 
-	for (let filename of filenames) {
-		try {
-			const stats = await lstat(filename);
+	const statPromises = filenames.map((filename) => {
+		return lstat(filename);
+	});
 
-			console.log(filename, stats.isFile());
-		} catch (err) {
-			console.log(err);
+	const allStats = await Promise.all(statPromises);
+
+	for (let stats of allStats) {
+		const index = allStats.indexOf(stats);
+
+		if (stats.isFile()) {
+			console.log(filenames[index]);
+		} else {
+			console.log(chalk.blue.bgRed.bold(filenames[index]));
 		}
 	}
 });
-
-// method 1
-// const lstat = (filename) => {
-// 	return new Promise((resolve, reject) => {
-// 		fs.lstat(filename, (err, stats) => {
-// 			if (err) {
-// 				reject(err)
-// 			}
-
-// 			resolve(stats)
-// 		})
-// 	})
-// }
