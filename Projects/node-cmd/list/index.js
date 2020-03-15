7b465e04326a25da6032d6b13ad1efbe5c778134
Joshua Shaffer <jshaffer189@gmail.com>
2020-03-14 20:22:54 -0500

    updating node cmd

--- Projects/node-cmd/list/index.js
+++ Projects/node-cmd/list/index.js
@@ -6,18 +6,21 @@
 const fs = require('fs');
 const util = require('util');
 const chalk = require('chalk');
+const path = require('path');
 
 // const lstat = util.promisify(fs.lstat);
 
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
 	if (err) {
 		console.log(err);
 	}
 
 	const statPromises = filenames.map((filename) => {
-		return lstat(filename);
+		return lstat(path.join(targetDir, filename));
 	});
 
 	const allStats = await Promise.all(statPromises);
