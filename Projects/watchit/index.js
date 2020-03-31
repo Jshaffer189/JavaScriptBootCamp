a905a7b75945a5a2a19281ee3556a027a4c2b373
Joshua Shaffer <jshaffer189@gmail.com>
2020-03-30 20:36:41 -0500

    tools still working

--- Projects/watchit/index.js
+++ Projects/watchit/index.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+
+program.version('.0.0.1').argument('[filename]', 'Name of a file to execute').action((args) => {
+	console.log(args);
+});
+
+const start = debounce(() => {
+	console.log('STARTING USERS PROGRAM');
+}, 100);
 
 chokidar
 	.watch('.')
-	.on('add', () => console.log('FILE ADDED'))
+	.on('add', start)
 	.on('change', () => console.log('FILE CHANGED'))
 	.on('unlink', () => console.log('FILE UNLINKED'));
