73d197c089a7bb1b9074a57f250c82098cce15ef
Joshua Shaffer <jshaffer189@gmail.com>
2020-03-31 23:25:19 -0500

    cli tools

--- Projects/watchit/index.js
+++ Projects/watchit/index.js
@@ -3,17 +3,22 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
+const fs = require('fs');
 
-program.version('.0.0.1').argument('[filename]', 'Name of a file to execute').action((args) => {
-	console.log(args);
+program.version('.0.0.1').argument('[filename]', 'Name of a file to execute').action(async ({ filename }) => {
+	const name = filename || 'index.js';
+
+	try {
+		await fs.promises.access(name);
+	} catch (err) {
+		throw new Error(`Could not find the file ${name}`);
+	}
+
+	const start = debounce(() => {
+		console.log('STARTING USERS PROGRAM');
+	}, 100);
+
+	chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
 });
 
-const start = debounce(() => {
-	console.log('STARTING USERS PROGRAM');
-}, 100);
-
-chokidar
-	.watch('.')
-	.on('add', start)
-	.on('change', () => console.log('FILE CHANGED'))
-	.on('unlink', () => console.log('FILE UNLINKED'));
+program.parse(process.argv);
