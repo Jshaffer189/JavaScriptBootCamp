49c4695dca0b1459fccbb63e398d144c0a0539f2
Joshua Shaffer <jshaffer189@gmail.com>
2020-04-08 12:42:56 -0500

    updating repo

--- Projects/ecomm/repositories/users.js
+++ Projects/ecomm/repositories/users.js
@@ -14,7 +14,17 @@ class UsersRepository {
 		}
 	}
 
-	async checkForFile() {}
+	async getAll() {
+		return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
+	}
 }
 
-const repo = new UsersRepository('users.json');
+const test = async () => {
+	const repo = new UsersRepository('users.json');
+
+	const users = await repo.getAll();
+
+	console.log(users);
+};
+
+test();
