d896ca342911416a14c632e2da098a760deb870c
Joshua Shaffer <jshaffer189@gmail.com>
2020-04-09 23:33:11 -0500

    updating class methods

--- Projects/ecomm/repositories/users.js
+++ Projects/ecomm/repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
 	constructor(filename) {
@@ -17,14 +18,44 @@ class UsersRepository {
 	async getAll() {
 		return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
 	}
+
+	// account creation bundle function
+	async create(attrs) {
+		attrs.id = this.randomId();
+
+		const records = await this.getAll();
+		records.push(attrs);
+
+		await this.writeAll(records);
+	}
+
+	// writeAll
+	async writeAll(records) {
+		await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+	}
+
+	// randomID
+	randomId() {
+		return crypto.randomBytes(4).toString('hex');
+	}
+
+	// getOne
+	async getOne(id) {
+		const records = await this.getAll();
+		return records.find((record) => record.id === id);
+	}
+
+	// delete
+
+	// update
 }
 
 const test = async () => {
 	const repo = new UsersRepository('users.json');
 
-	const users = await repo.getAll();
+	const user = await repo.getOne('cc34e833');
 
-	console.log(users);
+	console.log(user);
 };
 
 test();
