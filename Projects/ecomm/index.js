614b2c487a7b8126c2fab94acc438a62f932eba2
Joshua Shaffer <jshaffer189@gmail.com>
2020-04-12 21:41:41 -0500

    user id implemented

--- Projects/ecomm/index.js
+++ Projects/ecomm/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 
 const app = express();
 
+// Node Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	cookieSession({
+		keys: [ '55d6agy8a6v4j4o5g6sc24s8' ]
+	})
+);
 
 // req is the browser object request
 // res is the server response to the user
 app.get('/', (req, res) => {
 	res.send(`
-        <div>
+		<div>
+			Your id is: ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email" />
                 <input name="password" placeholder="password" />
@@ -38,7 +46,8 @@ app.post('/', async (req, res) => {
 	// create a user in our user repo to represent this person
 	const user = await usersRepo.create({ email, password });
 
-	// store the id of that user inside the users cookie
+	// run a check if cookie session === users browser
+	req.session.userId = user.id;
 
 	res.send('Account created');
 });
