a58b203379e62158501cd96c4c1e6986ce9e1412
Joshua Shaffer <jshaffer189@gmail.com>
2020-04-20 16:31:29 -0500

    validators finished

--- Projects/ecomm/views/admin/auth/signup.js
+++ Projects/ecomm/views/admin/auth/signup.js
@@ -1,14 +1,30 @@
 const layout = require('../layout');
 
-module.exports = ({ req }) => {
+const getError = (errors, prop) => {
+	try {
+		return errors.mapped()[prop].msg;
+
+		// return errors object with [prop].error msg
+	} catch (err) {
+		return '';
+	}
+};
+
+module.exports = ({ req, errors }) => {
 	return layout({
 		content: `
         <div>
             Your id is: ${req.session.userId}
             <form method="POST">
-                <input name="email" placeholder="email"  />
+                <input name="email" placeholder="email"  /> 
+                ${getError(errors, 'email')}
+                <br>
                 <input name="password" placeholder="password" />
+                ${getError(errors, 'password')}
+                <br>
                 <input name="passwordConfirmation" placeholder="password confirmation" />
+                ${getError(errors, 'passwordConfirmation')}
+                <br>
                 <button>Sign up</button>
             </form>
         </div>
