6b6c28ec779f89a0e79f426de3c834f092543cdd
Joshua Shaffer <jshaffer189@gmail.com>
2020-04-29 21:55:35 -0500

    working on image upload

--- Projects/ecomm/routes/admin/products.js
+++ Projects/ecomm/routes/admin/products.js
@@ -1,9 +1,15 @@
+// external libs
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
+// internal exports
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
+
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 
@@ -11,14 +17,18 @@ router.get('/admin/products/new', (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [ requireTitle, requirePrice ], (req, res) => {
-	const errors = validationResult(req);
+router.post(
+	//
+	'/admin/products/new',
+	[ requireTitle, requirePrice ],
+	upload.single('image'),
+	(req, res) => {
+		const errors = validationResult(req);
 
-	req.on('data', (data) => {
-		console.log(data.toString());
-	});
+		console.log(req.file);
 
-	res.send('Submitted!');
-});
+		res.send('Submitted!');
+	}
+);
 
 module.exports = router;
