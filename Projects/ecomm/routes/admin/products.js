e236fad8834e9a9b0a41f684e46cf1aa36ef7f2a
Joshua Shaffer <jshaffer189@gmail.com>
2020-05-04 13:23:31 -0500

    updating photo and error handling

--- Projects/ecomm/routes/admin/products.js
+++ Projects/ecomm/routes/admin/products.js
@@ -7,6 +7,7 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
@@ -23,7 +24,7 @@ router.get('/admin/products/new', requireAuth, (req, res) => {
 });
 
 router.post(
-	//
+	// indenting
 	'/admin/products/new',
 	requireAuth,
 	upload.single('image'),
@@ -33,6 +34,40 @@ router.post(
 		const image = req.file.buffer.toString('base64');
 		const { title, price } = req.body;
 		await productsRepo.create({ title, price, image });
+
+		res.redirect('/admin/products');
+	}
+);
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+	const product = await productsRepo.getOne(req.params.id);
+
+	if (!product) {
+		return res.send('Product not found');
+	}
+
+	// product id
+	res.send(productsEditTemplate({ product }));
+});
+
+router.post(
+	'/admin/products/:id/edit',
+	// indenting
+	requireAuth,
+	upload.single('image'),
+	[ requireTitle, requirePrice ],
+	handleErrors(productsEditTemplate),
+	async (req, res) => {
+		const changes = req.body;
+		if (req.file) {
+			changes.image = req.file.buffer.toString('base64');
+		}
+
+		try {
+			await productsRepo.update(req.params.id, changes);
+		} catch (err) {
+			return res.send('Could not find item');
+		}
 
 		res.redirect('/admin/products');
 	}
