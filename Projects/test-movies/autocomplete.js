34dfcebba813d8699f91d4789475ae6592a68d83
Joshua Shaffer <jshaffer189@gmail.com>
2020-05-15 22:48:38 -0500

    starting build test framework

--- Projects/test-movies/autocomplete.js
+++ Projects/test-movies/autocomplete.js
@@ -1,11 +1,5 @@
-const createAutoComplete = ({
-  root,
-  renderOption,
-  onOptionSelect,
-  inputValue,
-  fetchData
-}) => {
-  root.innerHTML = `
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) => {
+	root.innerHTML = `
     <label><b>Search</b></label>
     <input class="input" />
     <div class="dropdown">
@@ -15,39 +9,39 @@ const createAutoComplete = ({
     </div>
   `;
 
-  const input = root.querySelector('input');
-  const dropdown = root.querySelector('.dropdown');
-  const resultsWrapper = root.querySelector('.results');
+	const input = root.querySelector('input');
+	const dropdown = root.querySelector('.dropdown');
+	const resultsWrapper = root.querySelector('.results');
 
-  const onInput = async event => {
-    const items = await fetchData(event.target.value);
+	const onInput = async (event) => {
+		const items = await fetchData(event.target.value);
 
-    if (!items.length) {
-      dropdown.classList.remove('is-active');
-      return;
-    }
+		if (!items.length) {
+			dropdown.classList.remove('is-active');
+			return;
+		}
 
-    resultsWrapper.innerHTML = '';
-    dropdown.classList.add('is-active');
-    for (let item of items) {
-      const option = document.createElement('a');
+		resultsWrapper.innerHTML = '';
+		dropdown.classList.add('is-active');
+		for (let item of items) {
+			const option = document.createElement('a');
 
-      option.classList.add('dropdown-item');
-      option.innerHTML = renderOption(item);
-      option.addEventListener('click', () => {
-        dropdown.classList.remove('is-active');
-        input.value = inputValue(item);
-        onOptionSelect(item);
-      });
+			option.classList.add('dropdown-item');
+			option.innerHTML = renderOption(item);
+			option.addEventListener('click', () => {
+				dropdown.classList.remove('is-active');
+				input.value = inputValue(item);
+				onOptionSelect(item);
+			});
 
-      resultsWrapper.appendChild(option);
-    }
-  };
-  input.addEventListener('input', debounce(onInput, 500));
+			resultsWrapper.appendChild(option);
+		}
+	};
+	input.addEventListener('input', debounce(onInput, 500));
 
-  document.addEventListener('click', event => {
-    if (!root.contains(event.target)) {
-      dropdown.classList.remove('is-active');
-    }
-  });
+	document.addEventListener('click', (event) => {
+		if (!root.contains(event.target)) {
+			dropdown.classList.remove('is-active');
+		}
+	});
 };
--- Projects/test-movies/test/autocomplete.test.js
+++ Projects/test-movies/test/autocomplete.test.js
@@ -1,5 +1,70 @@
-it('Shows an autocmplete', () => {
-	createAutoComplete({
-		root: document.querySelector('#target')
+const waitFor = (selector) => {
+	return new Promise((resolve, reject) => {
+		// if promise is rejected throw error and fail test
+		const interval = setInterval(() => {
+			if (document.querySelector(selector)) {
+				clearInterval(interval);
+				clearTimeout(timeout);
+				resolve();
+			}
+		}, 30);
+
+		const timeout = setTimeout(() => {
+			clearInterval(interval);
+			reject();
+		}, 2000);
 	});
+};
+
+// before each test refresh widget
+beforeEach(() => {
+	document.querySelector('#target').innerHTML = '';
+	createAutoComplete({
+		root: document.querySelector('#target'),
+		fetchData() {
+			return [
+				{
+					Title: 'Avengers'
+				},
+				{
+					Title: 'Not advengers'
+				},
+				{
+					Title: 'some other movie'
+				}
+			];
+		},
+		renderOption(movie) {
+			return movie.Title;
+		}
+	});
+});
+
+it('dropdown starts closed', () => {
+	const dropdown = document.querySelector('.dropdown');
+
+	expect(dropdown.className).not.to.include('is-active');
+});
+
+it('after seraching, dropdown opens up', async () => {
+	const input = document.querySelector('input');
+	input.value = 'advengers';
+	input.dispatchEvent(new Event('input'));
+
+	await waitFor('.dropdown-item');
+
+	const dropdown = document.querySelector('.dropdown');
+	expect(dropdown.className).to.include('is-active');
+});
+
+it('After Searching, displays some results', async () => {
+	const input = document.querySelector('input');
+	input.value = 'advengers';
+	input.dispatchEvent(new Event('input'));
+
+	await waitFor('.dropdown-item');
+
+	const items = document.querySelectorAll('.dropdown-item');
+
+	expect(items.length).to.equal(3);
 });
