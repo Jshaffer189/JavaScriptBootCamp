cdd5a8b01a6552788350e5f0ea93d8d4aa9e4979
Joshua Shaffer <jshaffer189@gmail.com>
2020-05-23 10:07:32 -0500

    unfinished

--- Projects/test-me/runner.js
+++ Projects/test-me/runner.js
@@ -1,12 +1,25 @@
 const fs = require('fs');
+const path = require('path');
 
 class Runner {
 	constructor() {
-		this.files = [];
+		// absolute file path for testFiles stored
+		this.testFiles = [];
 	}
 
 	async collectFiles(targetPath) {
 		const files = await fs.promises.readdir(targetPath);
+
+		for (let file of files) {
+			const filepath = path.join(targetPath, file);
+			const stats = await fs.promises.lstat(filepath);
+
+			if (stats.isFile() && file.includes('.test.js')) {
+				this.testFiles.push({ name: filepath });
+			} else if (stats.isDirectory()) {
+				const childFiles = await fs.promises.readdir(filepath);
+			}
+		}
 
 		return files;
 	}
